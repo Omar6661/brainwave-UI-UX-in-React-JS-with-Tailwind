0052f3dfe767030e05c0496be383c6b9e8279b22
omar <omarobakkali@gmail.com>
2024-07-29 18:50:39 +0100

    Skeleton Section Component

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,6 +5,7 @@ import Button from "../Components/Button";
 import MenuSvg from "../assets/svg/MenuSvg";
 import { HamburgerMenu } from "./design/Header";
 import { useState } from "react";
+import { disablePageScroll, enablePageScroll } from "scroll-lock";
 
 const Header = () => {
   const pathName = useLocation();
@@ -12,11 +13,15 @@ const Header = () => {
   const toggleNavigation = () => {
     if (openNavigation) {
       setOpenNavigation(false);
+      enablePageScroll();
     } else {
       setOpenNavigation(true);
+      disablePageScroll();
     }
   };
   const handleClick = () => {
+    if (!openNavigation) return;
+    enablePageScroll();
     setOpenNavigation(false);
   };
   return (
